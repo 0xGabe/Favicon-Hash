b3fc1719516365d3be02476831e2c8f2a05313ac
Gabriel-Lima232 <68028935+Gabriel-Lima232@users.noreply.github.com>
2022-09-11 15:47:15 -0300

    Create favicon.py

---
+++ favicon.py
@@ -0,0 +1,16 @@
+import mmh3
+import sys
+import codecs
+import requests
+
+if len(sys.argv) != 2:
+    print(f"How to use: {sys.argv[0]} [Favicon URL]")
+    sys.exit(0)
+
+try:
+    response = requests.get(sys.argv[1])
+    favicon = codecs.encode(response.content, 'base64')
+    hash = mmh3.hash(favicon)
+    print(f"[+] Favicon Hash: {hash}")
+except Exception as e:
+    print(f"[-] Error occured as: {e}", file=sys.stderr)
